c0929a29404fbde9cf3ec0938d7fddb60dd0891d
Muhammad alnablsi <mohammad.alny@yahoo.com>
2021-03-19 22:10:52 +0200

    deploy

--- lib/util/secrets.ts
+++ lib/util/secrets.ts
@@ -1,4 +1,6 @@
 export const SESSION_SECRET = '9r5Ba$VGPEoXrXvY$@MgFWUhsyq54#cjw!6';
 export const MONGODB_URI =
-	'mongodb+srv://resuuser83:yBYckNzaNV1KER8c@cluster0.tth9l.gcp.mongodb.net/appdata?retryWrites=true&w=majority';
+`mongodb+srv://${process.env.MONGO_USER}:${
+	process.env.MONGO_PASSWORD
+  }@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 export const JWT_SECRET = 'z2x6oQJ#BqWTvnEM2upUME4%x#Sg*9X39&P';
